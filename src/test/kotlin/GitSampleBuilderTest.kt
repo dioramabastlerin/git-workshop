585cc3b6303c710514560e796ee2e7e06b41cc41
bstachmann <egal>
2019-01-16 21:30:57 +0100

    Implement more helper functions for command execution.

--- src/test/kotlin/GitSampleBuilderTest.kt
+++ src/test/kotlin/GitSampleBuilderTest.kt
@@ -1,8 +1,7 @@
+import io.kotlintest.TestContext
 import io.kotlintest.matchers.beEmpty
 import io.kotlintest.matchers.collections.shouldContainExactly
-import io.kotlintest.matchers.haveSize
 import io.kotlintest.should
-import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import java.io.File
 import java.util.stream.Stream
@@ -11,33 +10,72 @@ import kotlin.streams.toList
 class GitSampleBuilderTest : StringSpec( {
 
     "sandbox"  {
-        GitSamplesBuilder().run {
+        GitSamplesBuilder(testDir()).run {
+            clear()
+            inDir("base") {
+                show("ls -lah ..")
+            }
+        }
+    }
+
+    "execute commands" {
+        GitSamplesBuilder(testDir()).run {
+            clear()
             execute("ls -1").toList() should beEmpty()
             execute("touch hallo welt")
             execute("ls -1 .").toList() shouldContainExactly listOf("hallo", "welt")
         }
+
     }
 
 })
 
 
+private fun TestContext.testDir() = File("build/tests/${this.description().name}")
+
+
 class GitSamplesBuilder(val workingDir: File = File("build/gitsamples")) {
 
+
     init {
-        if(workingDir.exists())
-            workingDir.deleteRecursively()
         workingDir.mkdirs()
     }
 
-    fun execute(command: String): Stream<String> {
+
+    fun clear() {
+        workingDir.deleteRecursively()
+        workingDir.mkdirs()
+    }
+
+
+    fun execute(command: String): Stream<String> = executeRaw(command, false).inputStream.bufferedReader().lines()
+
+    fun git(gitCommand: String): Stream<String> = execute("git $gitCommand")
+
+    fun show(command: String): Process = executeRaw(command, true)
+
+
+    private fun executeRaw(command: String, inheritStdout: Boolean): Process {
         val processBuilder = ProcessBuilder(command.split("""\s+""".toRegex()))
+
         processBuilder.directory(workingDir)
+
         processBuilder.redirectError(ProcessBuilder.Redirect.INHERIT)
+        if (inheritStdout) {
+            processBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT)
+        }
+
         val process = processBuilder.start()
+
         val exitCode = process.waitFor()
         if (exitCode != 0)
             throw Exception("Failed with exit code $exitCode: $command")
-        return process.inputStream.bufferedReader().lines()
+        return process
+    }
+
+
+    fun inDir(subDir: String, function: GitSamplesBuilder.() -> Unit) {
+        GitSamplesBuilder(File(workingDir, subDir)).run(function)
     }
 
 
