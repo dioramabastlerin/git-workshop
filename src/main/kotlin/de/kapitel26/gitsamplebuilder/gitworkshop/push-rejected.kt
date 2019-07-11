f802e521bb59f4adbbb4abf08e65633242069417
bstachmann <egal>
2019-07-12 00:28:05 +0200

    Improve exercise on push rejects.

--- src/main/kotlin/de/kapitel26/gitsamplebuilder/gitworkshop/push-rejected.kt
+++ src/main/kotlin/de/kapitel26/gitsamplebuilder/gitworkshop/push-rejected.kt
@@ -21,17 +21,86 @@ fun CollectionOfSamples.pushRejected() {
             git("push")
         }
 
-        inRepo {
-            editAndCommit("bar", 1)
+        doc("00-aufgabe.md") {
+            markdown("""
+                |# Übung - Umgang mit "Push Reject"
+                |
+                |Wenn mehrere Entwickler per `push` Commits
+                |in das selbe Repository hochladen kommt es
+                |häufig zu sogenannten "Push Rejects"
+                |
+                |In diesem Beispiel arbeitest Du in einem Klon `repo`
+                |des Projekts `blessed.git`.
+                |
+                |Auch eine andere Entwicklerin, *Anja*,
+                |arbeitet mit einem Klon von `blessed.git`.
+                |Wenn *Anja* ihre Änderungen vor Dir hochlädt,
+                |wirst Du einen "Push Reject" erfahren.
+            """)
+        }
+
+
+        createAufgabe(
+                "Lokal Commit(s) erstellen", """
+                    |Bearbeite die Datei `bar` und erstelle (mindestens) ein Commit mit den Änderungen.
+                    |Überprüfe danach mit `git status`, ob der Workspace sauber ist.
+        """) {
+            inRepo {
+                editAndCommit("bar", 1)
+                git("status")
+            }
         }
 
         createAufgabe(
-                "Änderungen holen", """
-                    Hole die beiden neuen Commits vom `origin`-Repository,
-                    ohne den lokalen `master` zu verändern.
+                "Push versuchen", """
+                    |Versuche jetzt die Änderung zu pushen.
+        """) {
+            inRepo {
+                git("push", setOf(1))
+                markdown("""
+                    |Wie Du siehst, der Push wurde verweigert. 
+                    |Anscheinend war *Anja* schneller,
+                    |und hat ihre Änderungen zuerst nach `blessed.git` gebracht.""")
+            }
+        }
+
+        createAufgabe(
+                "(optional) Problem analysieren", """
+                    | Hole zunächt die Änderungen, ohne zu integrieren (`fetch`),
+                    | und lasse Dir die Änderungen von *Anja* zeigen.
+        """) {
+            inRepo {
+                git("fetch")
+                git("diff --stat HEAD origin/master")
+                git("diff --stat HEAD...origin/master")
+            }
+        }
+
+        createAufgabe(
+                "Fremde Änderungen integrieren", """
+                    |
+        """) {
+            inRepo {
+                git("pull")
+                markdown("""
+                    |Da *Anja* eine andere Datei (`foo`) bearbeitet hat,
+                    |konnten ihre Änderungen problemlos integriert werden.
+                    |Man sieht, dass ein neues Commit entstanden ist,
+                    |welches die Stränge zusammenführt.
+                """)
+                git("log --graph --oneline")
+            }
+        }
+
+        createAufgabe(
+                "Erneut pushen", """
+                    |
         """) {
             inRepo {
                 git("push")
+                markdown("""
+                    |Und siehe da: Jetzt klappt's.
+                """)
             }
         }
 
