eac2dea9c2b5b51397df4d4f12582dc629acc5c0
bstachmann <egal>
2020-05-27 21:53:46 +0200

    Implement staging sample.

--- src/main/kotlin/de/kapitel26/gitsamplebuilder/gitworkshop/gitworkshopsandbox.kt
+++ src/main/kotlin/de/kapitel26/gitsamplebuilder/gitworkshop/gitworkshopsandbox.kt
@@ -12,13 +12,14 @@ fun main() {
 
             // repositoryUntersuchen()
             // commitsErstellen()
+            staging()
             // cloning()
             // pushFetchPull()
             // pushRejected()
             // integrationOfChanges()
             // rebasing()
             // branching()
-            modules()
+            // modules()
         }
     }
 }
