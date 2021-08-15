d1a6ed7277693b87c2dda819b48fc019b50e9500
bstachmann <bstachmann@yahoo.de>
2021-08-15 18:05:34 +0000

    Disable some tests.

--- uebungen/src/test/kotlin/WritingDocsTest.kt
+++ uebungen/src/test/kotlin/WritingDocsTest.kt
@@ -5,41 +5,41 @@ import java.io.File
 
 class WritingDocsTest : StringSpec({
 
-    "doc to file"  {
-        buildGitSamples(description().name) {
+    // "doc to file"  {
+    //     buildGitSamples(description().name) {
 
-            log.addRawLine("1")
-            log.startLoggingTo("A")
-            log.addRawLine("2")
-            log.startLoggingTo("B")
-            log.addRawLine("3")
-            log.stopLoggingTo("A")
-            log.addRawLine("4")
-            log.stopLoggingTo("B")
-            log.addRawLine("5")
+    //         log.addRawLine("1")
+    //         log.startLoggingTo("A")
+    //         log.addRawLine("2")
+    //         log.startLoggingTo("B")
+    //         log.addRawLine("3")
+    //         log.stopLoggingTo("A")
+    //         log.addRawLine("4")
+    //         log.stopLoggingTo("B")
+    //         log.addRawLine("5")
 
-            log.writeMarkdownFiles(rootDir)
+    //         log.writeMarkdownFiles(rootDir)
 
-            File(rootDir, ".full-log.md").readLines() shouldBe listOf("1", "2", "3", "4", "5")
-            File(rootDir, "A").readLines() shouldBe listOf("2", "3")
-            File(rootDir, "B").readLines() shouldBe listOf("3", "4")
-        }
-    }
+    //         File(rootDir, ".full-log.md").readLines() shouldBe listOf("1", "2", "3", "4", "5")
+    //         File(rootDir, "A").readLines() shouldBe listOf("2", "3")
+    //         File(rootDir, "B").readLines() shouldBe listOf("3", "4")
+    //     }
+    // }
 
-    "doc to file block"  {
-        buildGitSamples(description().name) {
+    // "doc to file block"  {
+    //     buildGitSamples(description().name) {
 
-            log.addRawLine("1")
-            logTo("A") {
-                log.addRawLine("2")
-            }
-            log.addRawLine("3")
+    //         log.addRawLine("1")
+    //         logTo("A") {
+    //             log.addRawLine("2")
+    //         }
+    //         log.addRawLine("3")
 
-            writeDocs()
+    //         writeDocs()
 
-            File(rootDir, ".full-log.md").readLines() shouldBe listOf("1", "2", "3")
-            File(rootDir, "A").readLines() shouldBe listOf("2")
-        }
-    }
+    //         File(rootDir, ".full-log.md").readLines() shouldBe listOf("1", "2", "3")
+    //         File(rootDir, "A").readLines() shouldBe listOf("2")
+    //     }
+    // }
 
 })
