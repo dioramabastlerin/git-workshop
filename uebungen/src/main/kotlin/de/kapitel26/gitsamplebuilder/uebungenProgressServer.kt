54e820520e2a255472a314fec2013c85fe0fd78d
bstachmann <bstachmann@yahoo.de>
2021-08-19 21:19:32 +0000

    first throughstitch

--- uebungen/src/main/kotlin/de/kapitel26/gitsamplebuilder/uebungenProgressServer.kt
+++ uebungen/src/main/kotlin/de/kapitel26/gitsamplebuilder/uebungenProgressServer.kt
@@ -1,18 +1,16 @@
 package de.kapitel26.gitsamplebuilder
 
-import io.ktor.application.call
+import com.fasterxml.jackson.module.kotlin.*
 import io.ktor.application.Application
-import io.ktor.response.respondTextWriter
+import io.ktor.application.call
+import io.ktor.html.respondHtml
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import io.ktor.html.respondHtml
-import kotlinx.html.*
-import com.fasterxml.jackson.module.kotlin.*  
 import java.io.File
-import kotlin.text.toIntOrNull
 import kotlin.math.abs
+import kotlinx.html.*
 
 fun main() {
     val adminServer = embeddedServer(Netty, port = 8040) { adminModule() }
@@ -22,37 +20,33 @@ fun main() {
 }
 
 data class Progress(
-    val aufgaben: List<Pair<String, List<String>>>,
-    val aktuelleAufgabe: Pair<String, List<String>>,
-    val participants: Map<String,String>
+        val aufgaben: List<Pair<String, List<String>>>,
+        val aktuelleAufgabe: Pair<String, List<String>>,
+        val participants: Map<String, String>,
+        val achievements: Map<String, Set<String>>
 )
 
 val progressFile = File("build/git-uebungen/progress.json")
 
 val mapper = jacksonObjectMapper()
 
-var state : Progress = 
-    if(progressFile.exists()) {
-        println("Restoring state from file")
-        mapper.readValue(progressFile)
-    } else {
-        println("Fresh startup")
-        readAufgaben().let { aufgaben ->
-            Progress(   
-                aufgaben,
-                aufgaben.first(),
-                emptyMap()
-            )    
-        }    
-    }
+var state: Progress =
+        if (progressFile.exists()) {
+            println("Restoring state from file")
+            mapper.readValue(progressFile)
+        } else {
+            println("Fresh startup")
+            readAufgaben().let { aufgaben ->
+                Progress(aufgaben, aufgaben.first(), emptyMap(), emptyMap())
+            }
+        }
 
-    
 fun update(newState: Progress) {
     state = newState
     mapper.writerWithDefaultPrettyPrinter().writeValue(progressFile, newState)
 }
 
-fun readAufgaben() : List<Pair<String, List<String>>> {
+fun readAufgaben(): List<Pair<String, List<String>>> {
     val s: String = File("build/git-uebungen/aufgaben.json").readText()
     return mapper.readValue(s)
 }
@@ -62,18 +56,43 @@ fun Application.participantsModule() {
         get("/") {
             call.respondHtml {
                 val userId = call.parameters["id"]
+                val toggleSid = call.parameters["toggle"]
+                if (toggleSid != null && userId != null) {
+                    val olda: Set<String> = state.achievements[toggleSid] ?: emptySet()
+                    update(
+                            if (olda.contains(userId)) {
+                                state.copy(
+                                        achievements =
+                                                state.achievements + (toggleSid to (olda - userId))
+                                )
+                            } else {
+                                state.copy(
+                                        achievements =
+                                                state.achievements + (toggleSid to (olda + userId))
+                                )
+                            }
+                    )
+                }
 
                 body {
                     h1 { text("Git Workshop - Progress Monitor") }
-                    if(userId == null) {
+                    if (userId == null) {
                         form(action = "/register", method = FormMethod.get) {
                             label { text("Dein Name/Alias:") }
-                            input(name = "newAlias") {value = "torfnase"}
+                            input(name = "newAlias") { value = "torfnase" }
                         }
                     } else {
                         h2 { text("Hallo ${state.participants[userId]}!") }
-                        h2 { text("Aufgabe ${state.aktuelleAufgabe.first}" ) }
-                        state.aktuelleAufgabe.second.forEach { p { text(it) } }        
+                        h2 { text("Aufgabe ${state.aktuelleAufgabe.first}") }
+                        state.aktuelleAufgabe.second.forEach { schritt ->
+                            p {
+                                val sid =
+                                        abs((state.aktuelleAufgabe.first to schritt).hashCode())
+                                                .toString()
+                                text(schritt)
+                                a(href = "/?id=$userId&toggle=$sid") { +(if(state.achievements[sid]?.contains(userId) ?: false) " erledigt" else " offen"  ) }
+                            }
+                        }
                     }
                 }
             }
@@ -81,20 +100,17 @@ fun Application.participantsModule() {
 
         get("/register") {
             call.respondHtml {
-                val newAlias = call.parameters["newAlias"] ?: throw Exception("Missing parameter newAlias")
+                val newAlias =
+                        call.parameters["newAlias"] ?: throw Exception("Missing parameter newAlias")
                 val newUserId = abs("participant/$newAlias".hashCode()).toString()
-                    if( state.participants[newUserId] == null) {
-                        update(state.copy(participants = state.participants +(newUserId to newAlias)))
-                        
-                        head {
-                            meta(content = "0; url=/?id=$newUserId") { httpEquiv="refresh"}
-                        }
-                    } else {
-                        body {
-                            p { +"Alias $newAlias ist bereits vergeben."}
-                        }
-                    }    
-           }
+                if (state.participants[newUserId] == null) {
+                    update(state.copy(participants = state.participants + (newUserId to newAlias)))
+
+                    head { meta(content = "0; url=/?id=$newUserId") { httpEquiv = "refresh" } }
+                } else {
+                    body { p { +"Alias $newAlias ist bereits vergeben." } }
+                }
+            }
         }
     }
 }
@@ -103,36 +119,44 @@ fun Application.adminModule() {
     routing {
         get("/") {
             val selected = call.parameters["select"] ?: "?"
-            update(state.copy(aktuelleAufgabe = state.aufgaben.find { it.first == selected } ?: state.aufgaben.first()))
+            update(
+                    state.copy(
+                            aktuelleAufgabe = state.aufgaben.find { it.first == selected }
+                                            ?: state.aufgaben.first()
+                    )
+            )
             call.respondHtml {
                 body {
                     h1 { text("Git Workshop - Progress Monitor") }
                     h2 { text("Aktuelle Aufgabe") }
+                    val totalNum = state.participants.size
                     state.aktuelleAufgabe.also { (aufgabe, schritte) ->
-                        schritte.forEach {
-                            p { +"AA $aufgabe/$it" } 
+                        schritte.forEach { schritt ->
+                            val sid =
+                                    abs((state.aktuelleAufgabe.first to schritt).hashCode())
+                                            .toString()
+                            p { +"$aufgabe/$schritt ${state.achievements[sid]?.size}/$totalNum ${state.achievements[sid]?.map { state.participants[it] }}" }
                         }
                     }
                     h2 { text("Aufgaben") }
                     form(action = "/", method = FormMethod.get) {
-                        state.aufgaben.map { it.first }
-                            .forEach {
-                                input(name = "select") {  
-                                    value= it
-                                    type = InputType.radio
-                                    checked = it == state.aktuelleAufgabe.first
-                                    onChange = "this.form.submit()"
-                                    text(it)
-                                }
-                                br {}
+                        state.aufgaben.map { it.first }.forEach {
+                            input(name = "select") {
+                                value = it
+                                type = InputType.radio
+                                checked = it == state.aktuelleAufgabe.first
+                                onChange = "this.form.submit()"
+                                text(it)
                             }
-                    }
-                    h2 { text("Teilnehmer")}
-                    p{
-                        state.participants.forEach { (userId, alias) ->
-                            + "$alias /?id=$userId"
                             br {}
-                       }   
+                        }
+                    }
+                    h2 { text("Teilnehmer") }
+                    p {
+                        state.participants.forEach { (userId, alias) ->
+                            +"$alias /?id=$userId"
+                            br {}
+                        }
                     }
                 }
             }
