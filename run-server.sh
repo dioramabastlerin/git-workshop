e65771586a07cf9a22b647ebc6fca1820c426cd4
bstachmann <bstachmann@yahoo.de>
2021-10-17 14:57:44 +0000

    Improve prebuild and startup

--- run-server.sh
+++ run-server.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-bundle exec jekyll serve -H localhost &
-sleep 5
-
-echo -e "\n\nRun the GitPod local companion (https://www.gitpod.io/blog/local-app)\nthen open http://localhost:4000"
+bundle exec jekyll serve -H localhost
