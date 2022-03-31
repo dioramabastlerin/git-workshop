2397571e501216d88b855f013574e5fa156b2690
bstachmann <bstachmann@yahoo.de>
2022-03-31 16:46:27 +0000

    improve uebung

--- uebungen/src/main/kotlin/de/kapitel26/gitsamplebuilder/gitworkshop/repository-untersuchen.kt
+++ uebungen/src/main/kotlin/de/kapitel26/gitsamplebuilder/gitworkshop/repository-untersuchen.kt
@@ -18,13 +18,19 @@ fun CollectionOfSamples.repositoryUntersuchen() {
                 * `git show <some-commit>` zeigt Details zu einem Commit
                 * Mit `~` Adressiert man Vorgänger eines Commits, 
                   z. B. ist `HEAD~2` der Vorvorgänger von `HEAD`.
-                * `git branch` und `git tag` listen vorhande Branches und Tags auf.
+                * `git branch` und `git tag` listen vorhandene Branches und Tags auf.
+                * Mit `git switch <branch-name>` kann man auf andere Branches wechseln.
+                * Mit `git switch --detach <commit>` kann man auf beliebige Versionen wechseln.
                 * Mit `blame` findet man heraus,in welchen Commit Zeilen zuletzt bearbeitet wurden.
                   - `-M` ermittelt Verschiebungen innerhalb einer Datei. 
                   - `-w` erkennt Zeilen wieder, auch wenn Whitespacing verändert wurde.
                   - `--show-number` zeigt vorherige Zeilennummern.
                   - `-C` ermittelt Kopien/Verschiebungen aus Dateien im selben  Commit, in dem die Zeile bearbeitet wurde,
                     `-C -C -C` sogar aus beliebigen Dateien.
+                * Mit `git restore -s <commit> -- <datei-oder-pfad>` kann man *Inhalte* beliebiger Versionen 
+                  von Dateien/Pfaden in den Workspace holten. Es wird dabei nicht auf das angegebenen Commit
+                  gewechselt, sondern nur Dateiinhalte in den Workspace geholt. Die betroffenen Dateien 
+                  werden als `modified` angezeigt und können Commited werden.
                    
                 # Setup
     
@@ -36,7 +42,7 @@ fun CollectionOfSamples.repositoryUntersuchen() {
 
 
             createRepo {
-                createFileAndCommit("hallo-welt")
+                createFileAndCommit("hallo-welt") { content = "Hallo Welt!" }
                 createDir("foo") {
                     createFileAndCommit("bar") {
                         edit(1, content = "Eine wirklich ziemlich lange Zeile in der Datei 'bar'")
@@ -84,8 +90,8 @@ fun CollectionOfSamples.repositoryUntersuchen() {
                     edit(8, content = "Ende")                    
                 }
 
-                git("tag release1.0")
-                editAndCommit("hallo-welt", 3)
+                git("tag release1.0")  
+                editAndCommit("hallo-welt") { content = "Hello World!" }
                 inDir("foo") {
                     startBranch("feature-a") {
                         editAndCommit("bar", 7)
@@ -148,7 +154,8 @@ fun CollectionOfSamples.repositoryUntersuchen() {
                     
                     Gebe den Inhalt der Datei `bar`,  wie er im vorigen Commit war. aus.
                     
-                    Hole die (ganze) vorige Version in den Workspace, um sie näher zu untersuchen.
+                    Wechsle zum vorigen Commit, und untersuche, wie der Workspace dannn aussieht.
+                    Wechsle dann wieder auf `master` zurück.
                 """
             ) {
                 markdown("\n\nDiese Dateien gab es in `HEAD~1`:")
@@ -156,8 +163,9 @@ fun CollectionOfSamples.repositoryUntersuchen() {
                 markdown("\n\nUnd hier der Inhalt von `bar`:")
                 git("show HEAD~1:foo/bar")
                 markdown("\n\nUnd jetzt holen wir genau diese Version in den Workspace:")
-                git("checkout HEAD~1")
+                git("switch --detach HEAD~1")
                 ll()
+                git("switch master")
             }
 
             createAufgabe(
@@ -189,7 +197,27 @@ fun CollectionOfSamples.repositoryUntersuchen() {
                 markdown("Hier sieht eine Verschiebung aus der Datei `restaurant`.")
                 git("blame nachher -s -w -M -C -C -C")
                 markdown("Hier sieht man, dass Inhalte aus einer anderen Datei `foo/bar` kopiert wurden.")
-            }    
+            } 
+            
+            createAufgabe(
+                "⭐ Hole alten Stand einer einzelnen Datei zurück.", """
+                    Die Datei `hallo-welt` wurde nach dem `release1.0` bearbeitet.
+                    Dem Kunden gefällt das nicht. Stelle den alten Zustand mit
+                    einem neuen Commit wieder her. 
+                """
+            ) {
+                markdown("In `release1.0` sah es os aus:")
+                git("show release1.0:hallo-welt")
+                markdown("Jetzt sieh es so aus:")
+                git("show HEAD:hallo-welt")
+                markdown("Gezieltes zurückholen:")
+                git("restore -s release1.0 hallo-welt")
+                git("commit -am 'Zurückgeholt'")
+                markdown("Im Commit-Graphen sieht man, wo die Branches und Tag stehen:")
+                markdown("Jetzt sieh es so wieder so aus:")
+                git("show HEAD:hallo-welt")
+            }
+
        }
     }
 }
